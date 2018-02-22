9faa80767295eba82cc0668b4a04e6abbb60f7c8
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-22 06:07:29 -0800

    moved all beginning files into chapter 1 folder

---
+++ Chapter-01/12-Generalizations.java
@@ -0,0 +1,10 @@
+public class Generalizations {
+  public static void main(String[] args) {
+
+    // learning java
+    boolean isComplete = true;
+    int awesomeLevel = 121;
+    int epicLevel = awesomeLevel * 2;
+    System.out.println(epicLevel);
+  }
+}
