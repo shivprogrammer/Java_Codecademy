ae86e3640c53cef8073fb9c4beed8bb1a9985a71
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-22 06:11:06 -0800

    practicing with And and Or

---
+++ Chapter-02/01-And.java
@@ -0,0 +1,5 @@
+public class And {
+  public static void main(String[] args) {
+    System.out.println(true && false); // return false
+  }
+}
---
+++ Chapter-02/02-Or.java
@@ -0,0 +1,5 @@
+public class Or {
+  public static void main(String[] args) {
+    System.out.println(true || false); // return true
+  }
+}
