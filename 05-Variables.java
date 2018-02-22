eca290d9b42a7886da438c606531870fc8adbf0d
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-22 05:47:10 -0800

    defining variables in Variables.java

---
+++ 05-Variables.java
@@ -0,0 +1,16 @@
+// Another important feature of Java (and of many programming languages) is the ability to store values using variables.
+// 1) A variable stores a value
+// 2) In Java, all variables must have a specified data type.
+
+int myLuckyNumber = 7;
+
+// The variable MyLuckyNumber now stores the value 7 as an int type.
+// A semicolon ; is used to end all Java single code statements.
+
+public class Variables {
+  public static void main(String[] args) {
+    int myNumber = 42;
+    boolean isFun = true;
+    char movieRating = 'A';
+  }
+}
