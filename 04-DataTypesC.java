d7dd8f9d7de2161d059e2d3ef5c25670010ddffe
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-22 05:41:50 -0800

    added DataTypesC.java

---
+++ 04-DataTypesC.java
@@ -0,0 +1,9 @@
+// The char data type is used to represent single characters. That includes the keys on a keyboard that are used to produce text.
+// 1) char is short for character and can represent a single character.
+// 2) All char values must be enclosed in single quotes, like this: 'G'
+
+public class DataTypeC {
+  public static void main(String[] args) {
+    System.out.println('S');
+  }
+}
