417ed2a5df1eb8e78fde76bd33784885f3209172
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-22 23:18:17 -0800

    added Precendence comment set up

---
+++ Chapter-02/04-Precedence.java
@@ -0,0 +1,7 @@
+/*
+Boolean opeartors follow rules that specify the order in which they are evaluated. This order is called Boolean operator precedence.
+The precedence of each Boolean operator is as follows:
+1. ! is evaluated first
+2. && is evaluated second
+3. || is evaluated third
+*/
