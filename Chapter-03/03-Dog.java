e226fb554c01fe559bfa54d78ff998cef5d6b349
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 00:27:18 -0800

    working with java constructor classes

---
+++ Chapter-03/03-Dog.java
@@ -0,0 +1,28 @@
+/*
+Let's start by creating the starting state of our class. We can do this by adding a class constructor to it.
+
+1. A class constructor will allow us to create Dog instances. With a class constructor, we can set some information about the Dog.
+2. If we do not create a class constructor, Java provides one that does not allow you to set initial information.
+
+The code below demonstrates how a class constructor is created:
+
+class Car {
+
+  // The class constructor for the Car class
+  public Car() {
+
+  }
+}
+
+In the example above, we created a class constructor for the Car class. This constructor will be used when we create Car instances later.
+*/
+
+class Dog {
+
+  public Dog() {
+  }
+
+  public static void main(String[] args) {
+  }
+
+}
