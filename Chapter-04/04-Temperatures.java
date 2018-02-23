4a7a16d8fc1263678b3d3d8b56ba55387b2e3df1
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 03:55:50 -0800

    practicing ArrayList manipulation

---
+++ Chapter-04/04-Temperatures.java
@@ -0,0 +1,22 @@
+/*
+ArrayList: Manipulation
+To add some values of type Integer to the ArrayList:
+ArrayList<Integer>
+quizGrades = new ArrayList<Integer>();
+quizGrades.add(95);
+quizGrades.add(87);
+quizGrades.add(83);
+*/
+
+import java.util.ArrayList;
+
+public class Temperatures {
+  public static void main(String[] args) {
+    ArrayList<Integer>
+    weeklyTemperatures = new ArrayList<Integer>();
+    weeklyTemperatures.add(78);
+    weeklyTemperatures.add(67);
+    weeklyTemperatures.add(89);
+    weeklyTemperatures.add(94);
+  }
+}
