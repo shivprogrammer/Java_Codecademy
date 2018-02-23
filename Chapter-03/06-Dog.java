0f8d483d2e8bc866a9352aa10caf4aaed5606bb6
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 00:59:28 -0800

    the main method in java

---
+++ Chapter-03/06-Dog.java
@@ -0,0 +1,18 @@
+/*
+The main Method
+The purpose of main:
+1. When Java runs your progam, the code inside of the main method is executed.
+*/
+
+class Dog {
+
+  int age;
+
+  public Dog(int dogsAge) {
+    age = dogsAge;
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
