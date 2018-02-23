822ee9b24ff9b9d2d03a26520004281a5707389f
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 00:13:46 -0800

    created the most basic instance of a java class

---
+++ Chapter-03/02-Dog.java
@@ -0,0 +1,11 @@
+/*
+One fundamental concept of object-oriented programming in Java is the class.
+A class is a set of instructions that describe how a data structure should behave.
+Java provides us with its own set of pre-defined classes, but we are also free to create our own custom classes.
+*/
+
+class Dog {
+  public static void main(String[] args) {
+    
+  }
+}
