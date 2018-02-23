3c9c3e998a28bb990ca64778f77a8a8df7863658
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 01:37:06 -0800

    more method practice with the Dog class

--- Chapter-03/10-Dog.java
+++ Chapter-03/10-Dog.java
@@ -25,3 +25,25 @@ class Car {
   }
 }
 */
+
+class Dog {
+  int age;
+
+  public Dog(int dogsAge) {
+    age = dogsAge;
+  }
+
+  public void bark() {
+    System.out.println("Woof!");
+  }
+
+  public void run(int feet) {
+    System.out.println("Your dog ran " + feet + " feet!");
+  }
+
+  public static void main(String[] args) {
+    Dog spike = new Dog(4);
+    spike.bark();
+    spike.run(1000);
+  }
+}
