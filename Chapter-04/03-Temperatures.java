72fa38ec46ec737ff926b25bbe24d108f98fcf65
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 03:46:59 -0800

    working with Array Lists

---
+++ Chapter-04/03-Temperatures.java
@@ -0,0 +1,19 @@
+/*
+The ArrayList stores a list of data of a specified type.
+ArrayList<Integer>
+quizGrades = new ArrayList<Integer>();
+
+ArrayList is a pre-defined Java class.
+To use it, we must first create an ArrayList object.
+
+In the example above, we create an ArrayList object called quizGrades that will store data types belonging to the <Integer> class (whole numbers).
+*/
+
+import java.util.ArrayList;
+
+public class Temperatures {
+  public static void main(String[] args) {
+    ArrayList<Integer>
+    weeklyTemperatures = new ArrayList<Integer>();
+  }
+}
