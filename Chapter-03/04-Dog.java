df54cea306753adcfe1b8cb5682607d0f8459bab
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 00:44:07 -0800

    instance variables in java

---
+++ Chapter-03/04-Dog.java
@@ -0,0 +1,29 @@
+/*
+Classes: Instance Variables
+When we create a new class, we probably have specific details that we cant the class to include. We save those specific details into instance variables.
+
+Here is an instance variable in the Car class that describes a detail that we might want to associate with a car:
+class Car {
+  // Using instance variables to model our Car class after a real-life car
+  int modelYear;
+
+  public Car() {
+
+  }
+}
+
+In the example above, we created the instance variable named modelYear. Instance variables model real-world car attributes, such as the model year of a car. Finally, the instance variable is represented by the int data type.
+*/
+
+class Dog {
+
+  int age;
+  public Dog() {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
