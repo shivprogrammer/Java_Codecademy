54945bf5ee7ee17081317253ca15645eb2d53fee
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 04:43:52 -0800

    getting some Java HashMap practice in

---
+++ Chapter-04/09-Restaurant.java
@@ -0,0 +1,25 @@
+/*
+HashMap
+
+Think of a HashMap as a real life dictionary. A dictionary contains a set of words and a definition for each word. A HashMap contains a set of keys and a value for each key.
+
+If we look up a word in a dictionary, we can get the definition. If you provide a HashMap with a key that exiss, you can retrieve the value associated with the key.
+
+Declaring a HashMap is shown in the following example:
+
+HashMap<String, Integer>
+myFriends = new HashMap<String, Integer>();
+
+In the example above, we create a HashMap object called myFriends. The myFriends HashMap will store keys of String data types and values of the type Integer.
+
+*Note: the String object allows you to store characters, such as a word in quotations (e.g. "Snapple!");
+*/
+
+import java.util.HashMap;
+
+public class Restaurant {
+  public static void main(String[] args) {
+    HashMap<String, Integer>
+    restaurantMenu = new HashMap<String, Integer>();
+  }
+}
