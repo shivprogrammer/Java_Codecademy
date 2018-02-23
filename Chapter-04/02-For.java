af3a264c5d3200adb0071cb2765dddb72629791e
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-23 03:37:40 -0800

    practicing For loops in Java

---
+++ Chapter-04/02-For.java
@@ -0,0 +1,8 @@
+public class For {
+
+  public static void main(String[] args) {
+    for (int waterLevel = 0; waterLevel < 7; waterLevel++) {
+      System.out.println("The pool's water level is at " + waterLevel + " feet.");
+    }
+  }
+}
